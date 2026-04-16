01a277ffe57ee24ad936a32eb5b18bab49893874
Big Dev <badejoemmanuel805@gmail.com>
2026-04-16 11:10:52 +0100

    -ammend

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,11 +1,11 @@
 import { Injectable,OnModuleInit,OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from '@prisma/client';
 import {Pool} from "pg"
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy  {
+export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy {
 
 constructor(configService:ConfigService){
     const pool = new Pool({
